fc433928d3ece8b7c31eaa61bea252600d8c3225
junee26 <sjunee5@gmail.com>
2023-02-16 15:24:38 +0545

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# enter three number in one line
+a, b, c = input("enter three number").split(" ")
+print(a,b,c);
+
+
+h = "harryPotter"
+p = "peterPan"
+
+print(h[0:5]+p[5:])
+
+print(h[0:5],p[5:])
